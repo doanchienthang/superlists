738c18183c6f5bcb4c2a6562586217badf8049a4
Doan Chien Thang <thangdoanchien@gmail.com>
2015-10-09 19:55:13 +0700

    Refactor list view to handle new item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    # url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
     url(r'^new$', 'lists.views.new_list', name='new_list'),
 ]
